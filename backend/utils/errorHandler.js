667e18e78c1a03ad4a3191d37ce80c9d76f1b1ce
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2024-03-18 23:53:09 +0530

    Delete backend directory

--- backend/utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = this.statusCode;
-
-    Error.captureStackTrace(this, this.construtor);
-  }
-}
-module.exports = ErrorHandler;
